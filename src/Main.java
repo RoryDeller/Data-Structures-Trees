5b6824ea5afcdf170799aec3eb1a48740b9a8e5d
61672 <rdeller.61672@farnborough.ac.uk>
2017-10-31 16:00:58 +0000

    Finished product

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 public class Main {
 
+    private static void printTree(Node node, int depth) {
+
+        for (int i = 0; i <= depth; i++) {
+            System.out.print("---");
+        }
+        System.out.println("> " + node.getValue());
+
+        for (Node child: node.getEdges()) {
+            printTree(child, depth+1);
+        }
+
+    }
     public static void main(String[] args){
+
         Node root = new Node("animals");
         Node vertebrates = new Node ( "Vertebrates");
+
         Node invertebrates = new Node ( "Invertebrates");
+        Node worm = new Node ("Worm");
+        Node amphibians = new Node ("Amphibians");
+        vertebrates.addEdge(amphibians);
+        invertebrates.addEdge(worm);
         root.addEdge(vertebrates);
         root.addEdge(invertebrates);
+        printTree(root, 0);
 
         Node mammals = new Node ("mammals"); // Leaf nodes
 
--- src/Node.java
+++ src/Node.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
             return value;
         }
 
+
     }
 
 
